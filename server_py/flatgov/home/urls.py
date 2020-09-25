7fc7b9c308f1a158b436b2e2172c04b8c2f06a0b
Ari Hershowitz <arihershowitz@gmail.com>
2020-09-25 08:08:20 -0700

    Typeahead working with bills

--- server_py/flatgov/home/urls.py
+++ server_py/flatgov/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('billsList.json', views.getBillsList, name='bills')
 ]
--- server_py/flatgov/home/views.py
+++ server_py/flatgov/home/views.py
@@ -1,4 +1,5 @@
 import os
+import json
 from django.shortcuts import render
 from django.conf import settings
 
@@ -6,11 +7,12 @@ CONGRESS_DATA_PATH = getattr(settings, "CONGRESS_DATA_PATH", None)
 BILL_LIST_PATH = os.path.join(CONGRESS_DATA_PATH, 'billList.json')
 
 with open(BILL_LIST_PATH, 'r') as f:
-    billsList = f.read() 
+    billsList = json.load(f) 
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.conf import settings
 from functools import reduce
 import json
@@ -20,6 +22,10 @@ from typing import Dict
 def index(request):
     return HttpResponse("Hello, world. You're at the home index.")
 
+def getBillsList(request):
+    context = {}
+    context['bill_list'] = billsList 
+    return JsonResponse(context)
+
 def home_view(request):
-    context = {'bill_list': billsList}
-    return render(request, 'home/home.html', context)
+    return render(request, 'home/home.html')

ddb6de4c44385dd995c5e3fff8f00603ee1baaff
Ari Hershowitz <arihershowitz@gmail.com>
2020-09-25 09:24:30 -0700

    Provide sample bills as alternative

--- server_py/flatgov/home/views.py
+++ server_py/flatgov/home/views.py
@@ -5,9 +5,13 @@ from django.conf import settings
 
 CONGRESS_DATA_PATH = getattr(settings, "CONGRESS_DATA_PATH", None) 
 BILL_LIST_PATH = os.path.join(CONGRESS_DATA_PATH, 'billList.json')
+SAMPLE_BILLS = "['116hr1', '116hr202', '116s200', '112hr100']"
 
-with open(BILL_LIST_PATH, 'r') as f:
-    billsList = json.load(f) 
+if os.file.exists(BILL_LIST_PATH):
+    with open(BILL_LIST_PATH, 'r') as f:
+        billsList = json.load(f) 
+else:
+    billsList = SAMPLE_BILLS 
 
 # Create your views here.
 

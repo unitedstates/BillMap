100ae1a160eb45a73a5708336acb34d2a682eba6
Ari Hershowitz <arihershowitz@gmail.com>
2020-09-26 12:18:29 -0700

    Update download script (working now)

--- scripts/download_bills.sh
+++ scripts/download_bills.sh
@@ -2,34 +2,72 @@
 
 START_CONGRESS=110
 CURRENT_CONGRESS=116
+CONGRESS_SEQ=$(seq $CURRENT_CONGRESS -1 $START_CONGRESS)
 
-# Test if ../../congress already exists and terminate with a warning if it does, to avoid overwriting
-if [ -d "../../congress" ] 
-then 
-    echo "Directory ../../congress already exists. This script would overwrite it; you must move or delete it before running this script."
-    exit 1
+LOCAL_TMP=tmp
+TARGET_CONGRESS_DIR=../../congress-new
+
+if [ -d "${LOCAL_TMP}" ] 
+then
+    echo "${LOCAL_TMP}/ found"
 else
-    echo "There is no previous directory at ../../congress. Preparing that directory now..."
+    echo "Creating ${LOCAL_TMP}/ for downloads"
+    mkdir "${LOCAL_TMP}"
 fi
 
+echo "Downloading data for Congresses: ${CONGRESS_SEQ}"
 
-for CONGRESS in $(seq $START_CONGRESS $CURRENT_CONGRESS)
+for CONGRESS in $CONGRESS_SEQ 
 do
-	echo "Downloading data for ${CONGRESS}th Congress"
-    curl https://s3.amazonaws.com/pp-projects-static/congress/bills/${CONGRESS}.zip -o ${CONGRESS}.zip
-
-	echo "Unzipping directory for ${CONGRESS}th Congress"
-    unzip ${CONGRESS}.zip
-
-    if [ $CONGRESS -eq $START_CONGRESS ]
+    if [ -f "${LOCAL_TMP}/${CONGRESS}.zip" ] 
     then
-	    echo "Moving directory for ${CONGRESS}th Congress"
-        mv congress/ ../../congress
+        echo "${LOCAL_TMP}/${CONGRESS}.zip already exists. Skipping...". 
     else
-	    echo "Moving directory for ${CONGRESS}th Congress into existing directory"
-        mv congress/data/${CONGRESS}/ ../../congress/data/
+        echo "Downloading data for ${CONGRESS}th Congress to ${LOCAL_TMP}/${CONGRESS}.zip"
+        curl https://s3.amazonaws.com/pp-projects-static/congress/bills/${CONGRESS}.zip -o ${LOCAL_TMP}/${CONGRESS}.zip
+
+        echo "Done downloading data for ${CONGRESS} to ${LOCAL_TMP}/${CONGRESS}.zip". 
     fi
-	
-    echo "Done downloading and setting up directory. You should have data from ProPublica in ../../congress"
 
 done
+
+for CONGRESS in $CONGRESS_SEQ 
+do
+	echo "Unzipping directory for ${CONGRESS}th Congress"
+    unzip "${LOCAL_TMP}/${CONGRESS}.zip"  -x "__MACOSX/*" -d "${LOCAL_TMP}/${CONGRESS}/"
+    echo "Done unpacking data from ${CONGRESS}. You can now find it in ${LOCAL_TMP}/${CONGRESS}"
+	
+done
+
+# Test if $TARGET_CONGRESS_DIR already exists and terminate with a warning if it does, to avoid overwriting
+if [ -d "${TARGET_CONGRESS_DIR}" ] 
+then 
+    echo "Directory ${TARGET_CONGRESS_DIR} already exists. This script would overwrite it; you must move or delete it before running this script."
+    exit 1
+else
+    echo "There is no previous directory at ${TARGET_CONGRESS_DIR}. Preparing that directory now..."
+    mkdir -p ${TARGET_CONGRESS_DIR}/data
+
+    # Handle directories that open to $CONGRESS (e.g. 115/) instead of congress/
+    for CONGRESS in $CONGRESS_SEQ
+    do
+        if [ -d "${LOCAL_TMP}/${CONGRESS}/congress/data/${CONGRESS}" ] 
+        then
+            mv "${LOCAL_TMP}/${CONGRESS}/congress/data/${CONGRESS}" "${TARGET_CONGRESS_DIR}/data/${CONGRESS}"
+        fi
+
+        if [ -d "${LOCAL_TMP}/${CONGRESS}/${CONGRESS}" ] 
+        then
+            mv "./${LOCAL_TMP}/${CONGRESS}/${CONGRESS}" "${TARGET_CONGRESS_DIR}/data/${CONGRESS}"
+        fi
+
+        if [ -d "${LOCAL_TMP}/${CONGRESS}/bills" ] 
+        then
+            mkdir "${TARGET_CONGRESS_DIR}/data/${CONGRESS}" 
+            mv "./${LOCAL_TMP}/${CONGRESS}/bills" "${TARGET_CONGRESS_DIR}/data/${CONGRESS}/"
+        fi
+    done
+    echo "Done. Data is now available in ${TARGET_CONGRESS_DIR}."
+    # TODO: check that directories have been made, then remove tmp
+    # rm -r ${LOCAL_TMP}
+fi

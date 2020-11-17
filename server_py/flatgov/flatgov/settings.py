930617f01b4505c23c6fdb472d0a49952d0390cd
Ari Hershowitz <arihershowitz@gmail.com>
2020-11-17 10:27:44 -0800

    Update dotenv and sample .env variables

--- server_py/flatgov/flatgov/settings.py
+++ server_py/flatgov/flatgov/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(verbose=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent

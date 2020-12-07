038660665663d7a4b105b4c626d415168fcd9834
kapphire <ken.sapphire87@yahoo.com>
2020-12-07 20:49:31 +1300

    datatable idisplaylength updated

--- server_py/flatgov/static/js/bill.js
+++ server_py/flatgov/static/js/bill.js
@@ -1,6 +1,9 @@
 $(document).ready( function () {
     $('#related-bills-table').DataTable({
         bFilter: true,
+        iDisplayLength: 100,
+        scrollY: '50vh',
+        scrollCollapse: true,
         language: {
             paginate: {
                 "previous": "<",
@@ -9,10 +12,13 @@ $(document).ready( function () {
             info: "_START_ to _END_ of _TOTAL_ Bills",
             lengthMenu: "_MENU_ bills",
         },
-        lengthMenu: [5, 10, 20],
+        lengthMenu: [100, 50, 20, 5],
     });
     $('#cosponsors-table').DataTable({
         bFilter: true,
+        iDisplayLength: 100,
+        scrollY: '50vh',
+        scrollCollapse: true,
         language: {
             paginate: {
                 "previous": "<",
@@ -21,10 +27,13 @@ $(document).ready( function () {
             info: "_START_ to _END_ of _TOTAL_ Cosponsors",
             lengthMenu: "_MENU_ cosponsors",
         },
-        lengthMenu: [5, 10, 20],
+        lengthMenu: [100, 50, 20, 5],
     });
     $('#similar-bills-table').DataTable({
         bFilter: true,
+        iDisplayLength: 100,
+        scrollY: '50vh',
+        scrollCollapse: true,
         language: {
             paginate: {
                 "previous": "<",
@@ -33,6 +42,6 @@ $(document).ready( function () {
             info: "_START_ to _END_ of _TOTAL_ Bills",
             lengthMenu: "_MENU_ bills",
         },
-        lengthMenu: [5, 10, 20],
+        lengthMenu: [100, 50, 20, 5],
     });
 } );

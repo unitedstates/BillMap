abc76c9e2cfd94eea4eeeb25dbc9c62211fd9697
David Lee <david.lee@kastling.com>
2020-12-23 01:06:14 -0500

    #83 - Adding style changes to look more like suggestion. Need to verify that services support congresses separately, maybe it can be prepended to use existing search.

--- server_py/flatgov/home/forms.py
+++ server_py/flatgov/home/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class QueryForm(forms.Form): #Note that it is not inheriting from forms.ModelForm
     auto_id = False
-    queryText = forms.CharField(label="", widget=forms.Textarea(attrs={"rows":8, "cols":60, "placeholder":"Enter text from a bill section you want to search."}))
+    queryText = forms.CharField(label="", widget=forms.Textarea(attrs={"rows":8, "class":"form-control", "placeholder":"Enter text for bills or resolutions"}))
     #All my attributes here

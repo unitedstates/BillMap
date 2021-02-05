ffa6073896be167ec1c5c6b20b4e62939b95d603
Ayesha Malika Khan <ayesha.malika.khan@gmail.com>
2021-02-05 16:28:46 -0500

    loading statements data in database

---
+++ server_py/flatgov/common/statements.py
@@ -0,0 +1,13 @@
+import json
+import requests
+from bills.models import Statement
+import os
+
+def load_statements():
+    print(Statement.objects.all().count())
+    Statement.objects.all().delete()
+        
+    with open('dumped_statements.json', 'r') as f:
+        statements_data =json.loads(f.read())
+        print(len(statements_data))
+    os.system('./manage.py loaddata dumped_statements.json')
--- server_py/flatgov/flatgov/urls.py
+++ server_py/flatgov/flatgov/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+a

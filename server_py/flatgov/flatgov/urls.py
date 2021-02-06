6da313dc7a86c71fac46be921f4f7aeb98c1374e
Ari Hershowitz <arihershowitz@gmail.com>
2021-02-06 10:14:06 -0800

    Feature/soa ayesha (#151)
    * Add documentation to load data and do migration
    
    * Add instructions to load data and perform migration
    
    * loading statements data in database
    
    * updated README
    
    Co-authored-by: ayeshamk <ayesha.malika.khan@gmail.com>

--- server_py/flatgov/flatgov/urls.py
+++ server_py/flatgov/flatgov/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('uscongress/debug/', include('uscongress.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+a

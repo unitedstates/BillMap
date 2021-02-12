2341ce7bf82ff27c6837eeefb65f2628bd49568e
Ari Hershowitz <arihershowitz@gmail.com>
2021-02-12 15:42:57 -0800

    Update docs re bill similarity

--- scripts/docs_generator.sh
+++ scripts/docs_generator.sh
@@ -3,5 +3,6 @@
 cd "${0%/*}"
 cd ..
 cp *.adoc ./server_py/flatgov/static/docs
+cp media/*.png ./server_py/flatgov/static/docs/media/
 asciidoctor ./server_py/flatgov/static/docs/*.adoc
 rm ./server_py/flatgov/static/docs/*.adoc

0ae7fb7b83afe7e17db0379b99ab63b78a9038c4
Ari Hershowitz <arihershowitz@gmail.com>
2021-02-15 21:52:27 -0800

    Feature/clean crs (#176)
    * Limit bill regex + do not create new bill if not found in database
    
    * Fix for crs migration

--- server_py/flatgov/crs/populate_crs_table.py
+++ server_py/flatgov/crs/populate_crs_table.py
@@ -7,24 +7,18 @@ crs_api.populate()
 """
 import re
 import math
-import json
 from bills.models import Bill
 from crs.scrapers.everycrsreport_com import EveryCrsReport
 
 # Bill's types {'sres', 'hjres', 'hconres', 's', 'hres', 'sjres', 'hr', 'sconres'}
-BILL_NUMBER_RE = re.compile(
-    r'\W(\s?h\s?j\s?res\s?\d+|s\s?res\s?\d+|h\s?con\s?res\s?\d+|h\s?res\s?\d+'
-    r'|s\s?j\s?res\s?\d+|h\s?r\s?\d+|s\s?con\s?res\s?\d+)', re.I | re.M)
-BILL_NUMBER_S_FOR_TITLE_RE = re.compile(r'\W(S\s?\d+)')
+BILL_NUMBER_RE = re.compile(r"\W((?:h\.\s?r\.|s\.|h\.conres\.|s\.conres\.|h\.\s?j\.\s?res\.|s\.\s?j\.\s?res\.|" 
++ r"h\.\s?res\.|s\.\s?res\.)\s?(?:[1-9]\d{0,2}))", re.I | re.M)
 
-# this is more strict to avoid matches with
-# `word ended with S and digit after` or with images like `+s3..`
-BILL_NUMBER_S_FOR_TEXT_RE = re.compile(r'\W(S\s?\d{3,4})')
-
+def cleanBillNumber(billnumber):
+    return billnumber.replace('.', '').replace(' ', '').lower()
 
 def get_congress_number_for_year(year: str) -> int:
     return math.ceil((int(year) - 1788) / 2)
-
 
 class CrsFromApi:
     matched_count = 0
@@ -52,17 +46,9 @@ class CrsFromApi:
                 print(f'{bill_id} was matched, use existing bill.')
                 self.matched_count += 1
             except Bill.DoesNotExist:
-                print(f'{bill_id} does not have a match in Bills. Creating new one.')
-                bill_type = re.search(r'([a-z]+)(\d+)', bill_id)[1]
-                bill = Bill(
-                    type=bill_type,
-                    number=re.search(r'([a-z]+)(\d+)', bill_id)[2],
-                    congress=congress_number,
-                    bill_congress_type_number=bill_id,
-                    titles=json.dumps([report.title]),
-                    es_similarity=json.dumps([]),
-                    es_similar_bills_dict=json.dumps({})
-                )
+                print(f'{bill_id} does not have a match in Bills.')
+                # Do no create bill if it is not found in db
+                continue
             bill.save()
             report.bills.add(bill)
 
@@ -73,13 +59,11 @@ class CrsFromApi:
             reports_count += 1
             print(report)
             report.save()
-            bill_numbers = BILL_NUMBER_RE.findall(report.title.replace('.', '')) + \
-                           BILL_NUMBER_S_FOR_TITLE_RE.findall(report.title.replace('.', ''))
+            bill_numbers = map(cleanBillNumber, BILL_NUMBER_RE.findall(report.title))
             if bill_numbers:
                 self.process_bills_for_report(bill_numbers, report, source='title')
             if report.report_content_raw:
-                bill_numbers = BILL_NUMBER_RE.findall(report.report_content_raw.replace('.', '')) + \
-                               BILL_NUMBER_S_FOR_TEXT_RE.findall(report.report_content_raw.replace('.', ''))
+                bill_numbers = map(cleanBillNumber, BILL_NUMBER_RE.findall(report.report_content_raw))
                 if bill_numbers:
                     self.process_bills_for_report(bill_numbers, report, source='text')
             report.save()  # call save after all bills will be added

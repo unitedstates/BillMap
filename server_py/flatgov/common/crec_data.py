3212b17a6d121f034bedb109be3e5a4ca4d4da99
ayeshamk <ayesha.malika.khan@gmail.com>
2021-03-09 11:38:19 -0500

    committee scraper updated

--- server_py/flatgov/common/crec_data.py
+++ server_py/flatgov/common/crec_data.py
@@ -12,32 +12,52 @@ def get_congress_number(date):
     const_congress = 117
     dif = const_year - int(year)
     congress = const_congress - (dif // 2)
-    print(dif)
-    print(const_congress)
     return congress
 
-def crec_loader():
-        
-    with open(settings.BASE_DIR / 'crec_data.json', 'r') as file:
-        data = json.loads(file.read())
+def json_validator():
+    with open(settings.BASE_DIR / 'crec_data.json', 'r') as a_file:
+        lines = a_file.readlines()
+        new_lines = []
+        for line in lines:
+            if line.split()[0] == '}{':
+                line = re.sub('}{', '},{', line)
+            new_lines.append(line)
+    with open(settings.BASE_DIR / 'crec_data.json', 'w') as f:
+        f.write("[\n")
+        for line in new_lines:
+            f.write('    '+line)
+        f.write("]\n")
 
-        for crec_data in data:
-            
-            try:
-                crec = CommitteeDocument()
-                crec.title = crec_data['title']
-                crec.category = crec_data['category']
-                crec.committee = crec_data['committee']
-                crec.report_number = ''.join(crec_data['report_number'].split('&nbsp;'))
-                crec.associated_legislation = ''.join(crec_data['associated_legislation'].split('&nbsp;'))
-                crec.original_pdf_link = crec_data['pdf_link']
-                crec.bill_number = re.sub('\ |\?|\.|\!|\/|\;|\:|\-|\(|\)', '', ''.join(crec_data['associated_legislation'].split('&nbsp;'))).lower()
-                crec.chamber = crec_data['report_type'].split()[0]
-                crec.report_type = crec_data['report_type']
-                crec.date = crec_data['date']
-                crec.congress = get_congress_number(crec_data['date'])
-                crec.save()
-            except Exception as e:
-                print('-----', e)
-                print(crec_data)
+
+def crec_loader():
+
+    try:
+        with open(settings.BASE_DIR / 'crec_data.json', 'r') as file:
+            data = json.loads(file.read())
+            print(len(data))
+    except Exception as e:
+        with open(settings.BASE_DIR / 'crec_data.json', 'r') as file:
+            print('---- file is not valid', e)
+            json_validator()
+            data = json.loads(file.read())
+
+
+    for crec_data in data:
+        
+        try:
+            crec = CommitteeDocument()
+            crec.title = crec_data['title']
+            crec.category = crec_data['category']
+            crec.committee = crec_data['committee']
+            crec.report_number = ''.join(crec_data['report_number'].split('&nbsp;'))
+            crec.associated_legislation = ''.join(crec_data['associated_legislation'].split('&nbsp;'))
+            crec.original_pdf_link = crec_data['pdf_link']
+            crec.bill_number = re.sub('\ |\?|\.|\!|\/|\;|\:|\-|\(|\)', '', ''.join(crec_data['associated_legislation'].split('&nbsp;'))).lower()
+            crec.chamber = crec_data['report_type'].split()[0]
+            crec.report_type = crec_data['report_type']
+            crec.date = crec_data['date']
+            crec.congress = get_congress_number(crec_data['date'])
+            crec.save()
+        except Exception as e:
+            print('-----', e)
         

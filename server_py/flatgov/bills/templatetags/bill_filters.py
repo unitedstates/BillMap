6b23c3d8ff4a51cfde3f70667e9dfc51f9485ce7
Ari Hershowitz <arihershowitz@gmail.com>
2021-03-19 17:53:48 -0700

    Feature/221 billnumber format (#238)
    * Filter to format bill numbers

---
+++ server_py/flatgov/bills/templatetags/bill_filters.py
@@ -0,0 +1,46 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from datetime import date, timedelta
+from common import constants
+
+
+# BILL_NUMBER_REGEX = r'(?P<congress>[1-9][0-9]*)(?P<stage>[a-z]+)(?P<number>[0-9]+)(?P<version>[a-z]+)?$'
+
+stagesFormat = {
+    'S': 'S.',
+    'HR': 'H.R.',
+    'HRES': 'H.Res.',
+    'HJRES': 'H.J.Res.',
+    'HCONRES': 'H.Con.Res.',
+    'SJRES': 'S.J.Res.',
+    'SCONRES': 'S.Con.Res.',
+    'SRES': 'S.Res.'
+}
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def billnumber_display(billnumber: str):
+    billMatch = constants.BILL_NUMBER_REGEX_COMPILED.match(billnumber) 
+    if billMatch and billMatch.groupdict():
+        billMatchGroups = billMatch.groupdict()
+        congress = billMatchGroups.get('congress', '')
+        stage = billMatchGroups.get('stage', '')
+        number = billMatchGroups.get('number', '')
+        stage_fmt = stagesFormat.get(stage.upper(), stage)
+        if congress != constants.CURRENT_CONGRESS:
+           billnumber_fmt = f' {stage_fmt} {number} ({congress})'
+        else:
+           billnumber_fmt = f' {stage_fmt} {number}'
+        return billnumber_fmt
+        
+    else:
+        return billnumber
+
+@register.filter
+def billnumbers_display(billnumbers: list):
+    if isinstance(billnumbers, str):
+        billnumbers = billnumbers.split(', ')
+    return [billnumber_display(billnumber) for billnumber in list(billnumbers)]

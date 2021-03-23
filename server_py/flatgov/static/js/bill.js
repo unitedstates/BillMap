12ef4fac6ae077e82c44247197c0bd1ae3f9b4e6
kapphire <ken.sapphire87@yahoo.com>
2021-03-23 22:43:18 +0800

    combine tabs for related and similar bills

--- server_py/flatgov/static/js/bill.js
+++ server_py/flatgov/static/js/bill.js
@@ -1,21 +1,4 @@
 $(document).ready( function () {
-    $('#related-bills-table').DataTable({
-        sDom: "Rlfrtip",
-        bFilter: true,
-        iDisplayLength: 100,
-        scrollY: '50vh',
-        scrollX: true,
-        scrollCollapse: true,
-        language: {
-            paginate: {
-                "previous": "<",
-                "next": ">",
-            },
-            info: "_START_ to _END_ of _TOTAL_ Bills",
-            lengthMenu: "_MENU_ bills",
-        },
-        lengthMenu: [100, 50, 20, 5],
-    });
     $('#cosponsors-table').DataTable({
         sDom: "Rlfrtip",
         bFilter: true,
@@ -33,6 +16,7 @@ $(document).ready( function () {
         },
         lengthMenu: [100, 50, 20, 5],
     });
+
     $('#similar-bills-table').DataTable({
         sDom: "Rlfrtip",
         // order: [[ 3, 'desc' ]],
@@ -60,4 +44,60 @@ $(document).ready( function () {
         },
         lengthMenu: [100, 50, 20, 5],
     });
-} );
+
+    $('#current-similar-bills-table').DataTable({
+        sDom: "Rlfrtip",
+        autoWidth: false,
+        columnDefs: [
+            { "width": "10%", "targets": 0 },
+            { "width": "10%", "targets": 1 },
+            { "width": "15%", "targets": 2 },
+            { "width": "30%", "targets": 3 },
+            { "width": "10%", "targets": 4 },
+            { "width": "10%", "targets": 5 },
+        ],
+        bSort: false,
+        bFilter: true,
+        iDisplayLength: 100,
+        scrollY: '50vh',
+        scrollX: true,
+        scrollCollapse: true,
+        language: {
+            paginate: {
+                "previous": "<",
+                "next": ">",
+            },
+            info: "_START_ to _END_ of _TOTAL_ Bills",
+            lengthMenu: "_MENU_ bills",
+        },
+        lengthMenu: [100, 50, 20, 5],
+    });
+    $('#similar-sections-table').DataTable({
+        sDom: "Rlfrtip",
+        autoWidth: false,
+        columnDefs: [
+            { "width": "50%", "targets": 0 },
+            { "width": "50%", "targets": 1 },
+        ],
+        bSort: false,
+        bFilter: true,
+        iDisplayLength: 100,
+        scrollY: '50vh',
+        scrollX: true,
+        scrollCollapse: true,
+        language: {
+            paginate: {
+                "previous": "<",
+                "next": ">",
+            },
+            info: "_START_ to _END_ of _TOTAL_ sections",
+            lengthMenu: "_MENU_ sections",
+        },
+        lengthMenu: [100, 50, 20, 5],
+    });
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+        $($.fn.dataTable.tables(true)).DataTable()
+           .columns.adjust();
+     });
+});

2bb89afb91002340e74a5decf4b81a6a64cc52f0
Ari Hershowitz <arihershowitz@gmail.com>
2021-04-01 10:43:52 -0700

    Fix celery settings and readme

--- server_py/flatgov/flatgov/celery.py
+++ server_py/flatgov/flatgov/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flatgov.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flatgov.dev')
 
 app = Celery('flatgov')
 app.config_from_object('django.conf:settings', namespace='CELERY')

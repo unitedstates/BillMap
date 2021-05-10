910809b9bfb39594d883e483a0cfa99ef0bf88bc
Ari Hershowitz <arihershowitz@gmail.com>
2021-05-10 10:10:10 -0700

    Revert "Feature/no data msg2 (#347)" (#352)
    This reverts commit 887fcd7e837dd83ae780ffdca6df52097490f74a.

--- server_py/flatgov/bills/views.py
+++ server_py/flatgov/bills/views.py
@@ -147,8 +147,7 @@ class BillDetailView(DetailView):
         context['es_similarity'] = self.object.es_similarity
         context['cosponsors_for_bills'] = self.get_cosponsors_for_same_bills()
         context['propublica_api_key'] = settings.PROPUBLICA_CONGRESS_API_KEY
-        context['no_data_message_start'] = "No relevant"
-        context['no_data_message_end'] = "are available"
+        context['no_data_message'] = "No data available for this table"
         return context
 
     def get_related_statements(self, **kwargs):

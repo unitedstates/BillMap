ff5dd856945c4ee00f1ca0d3c9053e44a1f987a3
Ari Hershowitz <arihershowitz@gmail.com>
2021-05-18 13:20:21 -0700

    Update download path for crs

--- server_py/flatgov/crs/urls.py
+++ server_py/flatgov/crs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from crs.views import csv_report, CSVDownloadView
 
 urlpatterns = [
-    path('csv-report/', CSVDownloadView.as_view(), name='csv-download'),
+    path('csv-download/', CSVDownloadView.as_view(), name='csv-download'),
 ]

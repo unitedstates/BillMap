241c711e2aea3caa608eefc8d6b1254fd69ebfdc
Ari Hershowitz <arihershowitz@gmail.com>
2021-06-16 07:56:17 -0700

    Remove extra column definitions. #449

--- server_py/flatgov/static/js/bill.js
+++ server_py/flatgov/static/js/bill.js
@@ -40,12 +40,10 @@ $(document).ready( function () {
             'excelHtml5'
         ],
         columnDefs: [
-            { "width": "10%", "targets": 0 },
-            { "width": "25%", "targets": 1 },
-            { "width": "10%", "targets": 2 },
+            { "width": "15%", "targets": 0 },
+            { "width": "35%", "targets": 1 },
+            { "width": "35%", "targets": 2 },
             { "width": "15%", "targets": 3 },
-            { "width": "10%", "targets": 4 },
-            { "width": "10%", "targets": 5 },
         ],
         bSort: true,
         aaSorting: [],
@@ -82,12 +80,10 @@ $(document).ready( function () {
             'excelHtml5'
         ],
         columnDefs: [
-            { "width": "10%", "targets": 0 },
-            { "width": "10%", "targets": 1 },
-            { "width": "15%", "targets": 2 },
-            { "width": "30%", "targets": 3 },
-            { "width": "10%", "targets": 4 },
-            { "width": "10%", "targets": 5 },
+            { "width": "15%", "targets": 0 },
+            { "width": "35%", "targets": 1 },
+            { "width": "35%", "targets": 2 },
+            { "width": "15%", "targets": 3 },
         ],
         bSort: false,
         aaSorting: [],

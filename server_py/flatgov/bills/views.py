fb805a6e294eda76a9b58dea29abe4ab108958a9
ayeshamk <ayesha.malika.khan@gmail.com>
2021-06-17 15:51:52 -0400

    showing bill numbers

--- server_py/flatgov/bills/views.py
+++ server_py/flatgov/bills/views.py
@@ -188,7 +188,7 @@ class BillDetailView(DetailView):
             if report.html_url:
                 html_everycrs = "https://www.everycrsreport.com/files/" + report.html_url
 
-            context_item = {"title": report.title, "date": report.date, "main_everycrs": main_everycrs, "html_everycrs": html_everycrs, "pdf_everycrs": pdf_everycrs }
+            context_item = {"title": report.title, "identical_bill_numbers": report.bills.all().values_list('bill_congress_type_number', flat=True), "date": report.date, "main_everycrs": main_everycrs, "html_everycrs": html_everycrs, "pdf_everycrs": pdf_everycrs }
             metadata = json.loads(report.metadata)
             versions = metadata.get('versions', [])
             if versions and len(versions) > 0:

0c1823ab097b57ad72f3d886db21554af938de56
ayeshamk <ayesha.malika.khan@gmail.com>
2021-08-31 21:43:04 +0400

    issue #548 updates completed

--- server_py/flatgov/common/biden_statements.py
+++ server_py/flatgov/common/biden_statements.py
@@ -9,20 +9,12 @@ import yaml
 def load_biden_statements():
     current_biden_statements = Statement.objects.filter(administration='Biden')
     url = 'https://raw.githubusercontent.com/unitedstates/statements-of-administration-policy/main/archive/46-Biden.yaml'
+    original_pdf_path = 'https://github.com/unitedstates/statements-of-administration-policy/tree/main/archive/'
     response = requests.get(url)
     response_data = yaml.safe_load(response.text)
     if current_biden_statements.count() >= len(response_data):
         print("There're no any new biden statement to process.")
         return True
-
-    os.chdir(settings.BASE_DIR)
-    statement_pdf_links = current_biden_statements.values('permanent_pdf_link',)
-
-    for statement_pdf_link in statement_pdf_links:
-        try:
-            os.remove(settings.MEDIA_ROOT / statement_pdf_link['permanent_pdf_link'])
-        except OSError:
-            pass
     current_biden_statements.delete()
 
     for i, meta_statement in enumerate(response_data):
@@ -34,7 +26,8 @@ def load_biden_statements():
         statement.original_pdf_link = meta_statement['fetched_from_url']
         statement.bill_number = meta_statement['bills'][0]
         statement.administration = 'Biden'
-        response = requests.get(meta_statement['fetched_from_url'], stream = True)
-        statement.permanent_pdf_link = File(response.raw, name=f"{statement.congress}/{statement.bill_number}/{meta_statement['fetched_from_url'].split('/')[-1]}")
+        statement.permanent_pdf_link = original_pdf_path + meta_statement['file']
         statement.save()
         print(i, statement)
+        print(statement.permanent_pdf_link)
+        break

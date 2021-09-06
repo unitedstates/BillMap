a471d523235862693c89b54bdebadf8908880b7f
ayeshamk <ayesha.malika.khan@gmail.com>
2021-09-07 00:31:13 +0400

    issue #483, #485 and SAP date format fixed

--- server_py/flatgov/home/urls.py
+++ server_py/flatgov/home/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 from django.views.decorators.cache import never_cache
 
 from . import views
+from .propublica_api_views import GetPressStatementsAPIView
 
 app_name = 'home'
 
@@ -20,6 +21,7 @@ api_urlpatterns = [
     path('home/bill-list/', never_cache(views.BillListAPIView.as_view()), name='bill-list'),
     path('home/bill-titles/<congressnumber>', never_cache(views.BillListTitleAPIView.as_view()), name='bill-titles'),
     path('home/bill-title/<bill>/', never_cache(views.GetBillTitleAPIView.as_view()), name='bill-title'),
+    path('press-statements/<congress>/<bill_type>/<bill_number>/', never_cache(GetPressStatementsAPIView.as_view()), name='press-statements'),
 ]
 
 urlpatterns += api_urlpatterns
